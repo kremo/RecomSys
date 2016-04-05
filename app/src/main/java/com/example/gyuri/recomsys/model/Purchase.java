4b61f2f3b414948da89f541ca0939e0956894f4e
Andras Somlai <andris.94@hotmail.com>
2016-04-05 13:25:00 +0200

    purchase read-write toString

--- app/src/main/java/com/example/gyuri/recomsys/model/Purchase.java
+++ app/src/main/java/com/example/gyuri/recomsys/model/Purchase.java
@@ -1,11 +1,16 @@
 package com.example.gyuri.recomsys.model;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * Created by Gyuri on 2016. 03. 31..
  */
 public class Purchase {
+    String purchaseObjectSeparator = "|-_-|";
+
     public Book book;
     public User user;
     public Date date;
@@ -15,5 +20,36 @@ public class Purchase {
         this.book = book;
         this.user = user;
         this.date = date;
+    }
+
+    public Purchase(String loadedStr){
+        readFromString(loadedStr);
+    }
+
+    public String writeToString(){
+        String str = new String();
+        str = str.concat(book.writeToString()+purchaseObjectSeparator);
+        str = str.concat(user.writeToString()+purchaseObjectSeparator);
+
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String purchaseDate = df.format(date);
+
+        str = str.concat(purchaseDate+purchaseObjectSeparator);
+
+        return str;
+    }
+
+    public void readFromString(String str){
+        String[] strings = str.split(purchaseObjectSeparator);
+        user = new User(strings[0]);
+        book = new Book(strings[1]);
+        String dateString = strings[2];
+        SimpleDateFormat formatter1 = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        try {
+            date = (Date)formatter1.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 }
